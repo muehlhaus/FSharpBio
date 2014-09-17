9f280ec9658047f72df79ee500fdeb9a6afe3648
goedels <goedels89@googlemail.com>
2014-09-17 14:27:00 +0200

    Add amino acid properties

--- src/FSharpBio/BioSequences.fs
+++ src/FSharpBio/BioSequences.fs
@@ -97,7 +97,290 @@ module BioSequences =
 
 
 
+    let hydrophobicity (aas:AminoAcidSeq<_>) = 
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.hydrophobicity
 
+    let hydropathicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.hydropathicity
+
+    let omh (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.omh
+    
+    let bulkiness (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.bulkiness
+
+    let granthamPolarity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.granthamPolarity
+
+    let recFactors (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.recFactors
+
+    let molecularWeight (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.molecularWeight
+
+    let bullHydrophobicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.bullHydrophobicity
+
+    let guyHydrophobicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.guyHydrophobicity
+
+    let miyazawaHydrophobicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.miyazawaHydrophobicity
+
+    let rosemanHydrophobicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.rosemanHydrophobicity
+
+    let hydrationPotential (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.hydrationPotential
+
+    let constantsHydrophob (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.constantsHydrophob
+
+    let indicesHydrophob (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.indicesHydrophob
+
+    let mobility (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.mobility
+
+    let tfaRtCoeff (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.tfaRtCoeff
+
+    let hplcPh2rtCoeff (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.hplcPh2rtCoeff
+
+    let molFraction2001BuriedRes (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.molFraction2001BuriedRes
+
+    let proportionOfRes95Buried (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.proportionOfRes95Buried
+
+    let atomicWeightRatioHeteroElem (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.atomicWeightRatioHeteroElem
+
+    let avgFlexibilityIndex (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.avgFlexibilityIndex
+
+    let betaSheet29 (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.betaSheet29
+
+    let alphaHelix (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.alphaHelix
+
+    let betaTurn (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.betaTurn
+
+    let normFrequencyAlphaHelix (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.normFrequencyAlphaHelix
+
+    let normFrequencyBetaTurn (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.normFrequencyBetaTurn
+
+    let antiparallelBetaConfPreference (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.antiparallelBetaConfPreference
+
+    let overallAaComp (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.overallAaComp
+
+    let relMutability (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.relMutability
+
+    let numberOfCodonsUniversalGenCode (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.numberOfCodonsUniversalGenCode
+
+    let zimmermanPolarity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.zimmermanPolarity
+
+    let refractivity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.refractivity
+
+    let normConsensusHydrophob (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.normConsensusHydrophob
+
+    let hydrophilicity (aas:AminoAcidSeq<_>) =
+        aas
+        |> Seq.map AminoAcids.toLiteral
+        |> Seq.map AminoAcidLiteral.Propteries.hydrophilicity
+
+    let avgSorroundingHydrophob (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.avgSorroundingHydrophob
+    
+    let lAlphaHydrophobicity (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.lAlphaHydrophobicity
+    
+    let fauchereHydrophobicity (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.fauchereHydrophobicity
+    
+    let freeEnergyofTransfer (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.freeEnergyofTransfer
+    
+    let membraneBuriedHelixParam (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.membraneBuriedHelixParam
+    
+    let contribHydrophobInteractionsToStability (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.contribHydrophobInteractionsToStability
+    
+    let antigenicity (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.antigenicity
+    
+    let hydrophilicityScaleHPLCRtTime (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.hydrophilicityScaleHPLCRtTime
+    
+    let hydrophobIndex (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.hydrophobIndex
+    
+    let hfbaRtCoeff (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.hfbaRtCoeff
+    
+    let transmembraneTendency (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.transmembraneTendency
+
+    let hplcPH7RtCoeff (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.hplcPH7RtCoeff
+
+    let molFractionAccessibleResidues (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.molFractionAccessibleResidues
+
+    let meanFractionalAreaLoss (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.meanFractionalAreaLoss
+
+    let avgBuriedArea (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.avgBuriedArea
+
+    let alphaHelix29 (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.alphaHelix29
+
+    let betaTurn29 (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.betaTurn29
+
+    let betaSheet (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.betaSheet
+
+    let coil (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.coil
+
+    let normFrequencyBetaSheet (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.normFrequencyBetaSheet
+
+    let totalBetaStrandConfPreference (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.totalBetaStrandConfPreference
+
+    let parallelBetaStrandConfPreference (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.parallelBetaStrandConfPreference
+
+    let aaCompUniSwissProt (aas:AminoAcidSeq<_>) =
+            aas
+            |> Seq.map AminoAcids.toLiteral
+            |> Seq.map AminoAcidLiteral.Propteries.aaCompUniSwissProt
 
 
 
