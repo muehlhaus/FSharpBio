8b7c0157285dcd509111f21335d8b3cdc6e6ad3a
muehlhaus <muehlhaus@bio.uni-kl.de>
2014-10-13 17:55:24 +0200

    Add IO.AgilentRaw
    Read microarray raw data from Agilent

--- src/FSharpBio/Statistics/Descriptive/QuantileNorm.fs
+++ src/FSharpBio/Statistics/Descriptive/QuantileNorm.fs
@@ -9,7 +9,7 @@ module QuantileNormalization =
     // http://en.wikipedia.org/wiki/Quantile_normalization
     // technique for making two distributions or more identical in statistical properties.
     // to normalize two or more distributions to each other, rank the original values and group them by rank, then set to the average of the original values.         
-    let quantileNorm (data: float seq seq) =
+    let quantileNorm (data : seq<#seq<float>>) =
         
         // Helper function to group RankedValue by rank and calculate average of orignal values
         let groupByAndAverage (input: Rank.RankedValue<float> seq) =
@@ -33,3 +33,29 @@ module QuantileNormalization =
         |> Seq.map (fun col -> 
                         col |> Seq.map (fun r -> rankValueMap.TryFindDefault nan r.RankIndex))
             
+
+
+    let quantileNormXY (x: float seq) (y: float seq) =
+        
+        // Helper function to group RankedValue by rank and calculate average of orignal values
+        let groupByAndAverage (input: Rank.RankedValue<float> seq) =
+            input
+            |> Seq.groupBy  (fun i -> i.RankIndex)
+            |> Seq.map (fun (key, values) -> key, values |> Seq.averageBy (fun r -> r.Value) )
+        
+        // Transform values to their ranks
+        let xRawRanks = Rank.breakByMean x 
+        let yRawRanks = Rank.breakByMean y
+        
+        // Calculate rank to average value mappin
+        let rankValueMap =
+            [xRawRanks;yRawRanks]
+            |> Seq.concat
+            |> groupByAndAverage
+            |> Map.ofSeq
+        
+        // Get normalized values based on the rank of the original values
+        let rx = xRawRanks |> Seq.map (fun r -> rankValueMap.TryFindDefault nan r.RankIndex)
+        let ry = yRawRanks |> Seq.map (fun r -> rankValueMap.TryFindDefault nan r.RankIndex)
+        (rx,ry)
+
