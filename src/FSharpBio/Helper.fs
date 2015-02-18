fce38d31b92f1867b6e4b390621ce3452f064887
Mühlhaus <muehlhaus@bio.uni-kl.de>
2015-02-18 17:04:43 +0100

    mzRemote update: getMzArray

--- src/FSharpBio/Helper.fs
+++ src/FSharpBio/Helper.fs
@@ -14,3 +14,16 @@ module Helper =
         match input with
             | Regex.RegexValue @"Cre(?<chromosom>[\d]*)\.g(?<locusId>[\d]*)\.t(?<spliceId>[\d]*)\.(?<version>[\d]*)" [ chromosom; locusId; _ ; _; ] -> Some( sprintf "Cre%s.g%s" chromosom locusId)
             | _ -> None
+
+
+
+    let tryGetPfamId (str) =
+        match str with
+            | Regex.RegexValue @"Pfam:(?<pfamId>PF[\d]+)" [ pfamId; ] -> Some( pfamId )
+            | _ -> None
+
+    let tryGetGOId (str) =
+        match str with
+            | Regex.RegexValue @"(?<goId>GO:[\d]+)" [ goId; ] -> Some( goId )
+            | _ -> None
+
